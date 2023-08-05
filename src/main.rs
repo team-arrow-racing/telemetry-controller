13cd151a3dc3a307ccbe57ef1e76d94f5f952f31
Andrew Carr <7977719+athisun@users.noreply.github.com>
2023-08-06 02:27:40 +1000

    fix typos

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 //!
 //! # Task priority assignment
 //!
-//! It would be more idomatic to have these assigned in a enum or some constants
+//! It would be more idiomatic to have these assigned in a enum or some constants
 //! but RTIC doesn't yet support variables (static or otherwise) in task
 //! definitions.
 //!
 //! | Priority | Use |
 //! | --- | --- |
 //! | 0 | `idle` task and background tasks. |
-//! | 1 (default) | General and asychronous tasks. |
+//! | 1 (default) | General and asynchronous tasks. |
 //! | 2 | Synchronous comms tasks. |
 //! | 3 | System critical tasks. |
 
